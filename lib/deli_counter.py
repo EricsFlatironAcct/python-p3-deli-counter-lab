409633fdf20e303cd59f257c681b6660ff5c68a1
Eric Krukowski <erickruk@amazon.com>
2023-07-04 03:08:04 -0400

    remove unneeded lines

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -8,14 +8,6 @@ def line(queue):
 def take_a_number(queue, name):
     queue.append(name)
     print(f"Welcome, {name}. You are number {len(queue)} in line.")
-    return queue
 
 def now_serving(queue):
-    if len(queue) ==0:
-       print("There is nobody waiting to be served!")
-       return queue
-    print(f"Currently serving {queue.pop(0)}.")
-    return queue
-
-# OTHER_DELI = ["Logan", "Avi", "Spencer"]
-# line(OTHER_DELI)
+   print("There is nobody waiting to be served!") if len(queue)==0 else print(f"Currently serving {queue.pop(0)}.")
